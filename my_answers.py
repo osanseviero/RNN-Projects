9bbfdef77b78a7160ee8cbeed9f9dc291e809302
Omar Sanseviero <osanseviero@users.noreply.github.com>
2017-06-25 00:08:29 -0500

    feat: Predict stock value using RNN

--- my_answers.py
+++ my_answers.py
@@ -12,7 +12,20 @@ def window_transform_series(series,window_size):
     # containers for input/output pairs
     X = []
     y = []
-
+    
+    # From 0 to p-t
+    for i in range(len(series) - window_size):  
+        inp = []
+        
+        # Pick next window_size elems
+        for j in range(i, i + window_size):
+            inp.append(series[j])
+            
+        # Add input and output
+        X.append(inp)
+        y.append(series[i+window_size])
+        
+        
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,15 +36,40 @@ def window_transform_series(series,window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+    # given - fix random seed - so we can all reproduce the same results on our default time series
+    np.random.seed(0)
 
+
+    # TODO: build an RNN to perform regression on our time series input/output data
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+
+    # build model using keras documentation recommended optimizer initialization
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+    # compile the model
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+
+    # run your model!
+    model.fit(X_train, y_train, epochs=1000, batch_size=50, verbose=0)
+
+    return model
+
+import string
 
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
     # find all unique characters in the text
-
+    allowed_chars = string.ascii_lowercase + ' ' + '!' + ',' + '.' + ':' + ';' + '?'
 
     # remove as many non-english characters and character sequences as you can 
+    for char in text:
+        if char not in allowed_chars:
+            text = text.replace(char, '')
+
+    # shorten any extra dead space created above
+    text = text.replace('  ',' ')
 
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
